5eb69ba40652caff8cdc20cc27e0d0d3a814f121
Owo Sugiana <sugiana@gmail.com>
2011-04-20 05:07:17 +0700

    migrasi username password

---
+++ migrasi/views.py
@@ -0,0 +1,23 @@
+from models import Users
+from google.appengine.ext import search
+
+from django.contrib.auth.models import User
+
+from django.views.generic.simple import direct_to_template
+
+
+def user(request):
+    users = Users.all().order('username')
+    d = []
+    for source in users:
+        target = User.objects.filter(username=source.username).get()
+        if target:
+            if target.username == 'admin':
+                target.set_password(source.passwd)
+                target.save()
+        else:
+            target = User.objects.create_user(source.username, source.email, source.passwd)
+        d.append([source, target])
+    return direct_to_template(request, 'user.html', {
+        'users': d 
+        })
